e4793004ca5a8f65247f032fb9c2b6915c76d53c
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2019-09-21 19:42:50 +0300

    Add files via upload

---
+++ SyhMhfzDosyaTasimaClass/SyhMhfzDosyaTasimaClass/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SyhMhfzDosyaTasimaClass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(ebubekirbastama.klasor_tasi(textBox1.Text, textBox2.Text));
+             MessageBox.Show(ebubekirbastama.Copy(textBox1.Text, textBox2.Text));
+        }
+    }
+}
---
+++ SyhMhfzDosyaTasimaClass/SyhMhfzDosyaTasimaClass/ebubekirbastama.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+
+namespace SyhMhfzDosyaTasimaClass
+{
+    public class ebubekirbastama
+    {
+        public static string klasor_tasi(string KaynakDosya, string HedefDosya)
+       {
+            try
+            {
+                Directory.Move(KaynakDosya, HedefDosya);
+            }
+            catch (IOException exp)
+            {
+                return exp.Message;
+            }
+            return "Taşıma İşlemi Tamamlandı.";
+       }
+        public static string Copy(string KaynakDosya, string HedefDosya)
+        {
+            var diSource = new DirectoryInfo(KaynakDosya);
+            var diTarget = new DirectoryInfo(HedefDosya);
+            return CopyAll(diSource, diTarget);
+        }
+        public static string CopyAll(DirectoryInfo KaynakDosya, DirectoryInfo HedefDosya)
+        {
+            try
+            {
+                Directory.CreateDirectory(HedefDosya.FullName);
+
+                foreach (FileInfo fi in KaynakDosya.GetFiles())
+                {
+                    fi.CopyTo(Path.Combine(HedefDosya.FullName, fi.Name), true);
+                }
+
+                foreach (DirectoryInfo diSourceSubDir in KaynakDosya.GetDirectories())
+                {
+                    DirectoryInfo nextTargetSubDir =
+                        HedefDosya.CreateSubdirectory(diSourceSubDir.Name);
+                    CopyAll(diSourceSubDir, nextTargetSubDir);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                return ex.Message;
+            }
+            return "Kopyalama İşlemi Tamam";
+        }
+    }
+}
